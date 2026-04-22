75afbd793e84607b0431799bd94c210483f55215
Stuart McNicholas <stuart.mcnicholas@york.ac.uk>
2026-04-22 15:09:45 +0100

    Ability to change server root.

--- src/components/MoorhenApp.tsx
+++ src/components/MoorhenApp.tsx
@@ -1,6 +1,10 @@
 import { useEffect } from "react";
+import { useRef } from 'react';
 import { MoorhenProvider } from "moorhen";
 import { MoorhenContainer } from "moorhen";
+import { webGL } from 'moorhen/types/mgWebGL';
+import { moorhen } from 'moorhen/types/moorhen';
+import { serverRoot }  from '../serverRoot';
 
 export const MoorhenExitMenu = (props: { exitCallback: () => void }) => {
     useEffect(() => {
@@ -10,7 +14,22 @@ export const MoorhenExitMenu = (props: { exitCallback: () => void }) => {
     return <span>Saving...</span>;
 };
 
-export const MoorhenApp = () => {
+export const MoorhenApp = (props) => {
+
+    let rootPrefix = ""
+    let urlPrefix = "/MoorhenAssets"
+    if((serverRoot as string)!=="./"){
+        rootPrefix = (serverRoot as string).replace(/\/$/g, '')
+        urlPrefix = rootPrefix+"/MoorhenAssets"
+    }
+
+    const glRef = useRef<webGL.MGWebGL | null>(null)
+    const commandCentre = useRef<moorhen.CommandCentre | null>(null)
+
+    const collectedProps = {
+        glRef, commandCentre, urlPrefix
+    }
+
     return (
         <MoorhenProvider>
             <MoorhenContainer />
